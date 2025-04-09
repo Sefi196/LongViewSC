5d41e1e84b41af128fa76559178276188bb940e0
Sefi Prawer <yairp@student.unimelb.edu.au>
2025-04-09 11:50:57 +1000

    bug fix

--- app.R
+++ app.R
@@ -1,11 +1,11 @@
 # app.R
+# Start the Shiny app
+print("LongViewSC Starting ....")
+print("please cite ...")
+
+source("app/global.R")
 source("app/ui.R")      # Path to your UI file
 source("app/server.R")  # Path to your server file
-source("app/global.R")
-
-# Start the Shiny app
-print("Starting ....")
-print("please cite ....")
 
 
 shinyApp(ui = ui, server = server)
